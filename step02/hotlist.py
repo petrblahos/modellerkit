69d7e4870916b60f433357738b02a4dbaa750c26
Petr Blahos <petrblahos@gmail.com>
2014-04-23 20:22:07 +0200

    hotlist with a selection

---
+++ step02/hotlist.py
@@ -0,0 +1,117 @@
+import logging
+
+
+LOGGER = logging.getLogger("hotlist")
+LOGGER.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s[%(name)s]%(levelname)s: %(message)s')
+ch.setFormatter(formatter)
+# add the handlers to the logger
+LOGGER.addHandler(ch)
+
+class HotBase(object):
+    def __init__(self):
+        self.listeners = []
+
+    def add_listener(self, listener):
+        """
+            Appends a listener to the listener list. The listeners are called
+            in the order in which they were added.
+        """
+        self.listeners.append(listener)
+
+    def _fire(self, name, key):
+        """
+            Called to fire an event with the given name and given key.
+        """
+        LOGGER.debug("FIRE: %s %s", name, key)
+        for listener in self.listeners:
+            try:
+                listener(self, name, key)
+            except Exception, dummy:
+                LOGGER.exception("Error firing %s to %s", name, listener)
+
+class HotList(HotBase):
+    """
+        A list that fires when changed.
+    """
+    def __init__(self, init_iterable=None):
+        super(HotList, self).__init__()
+        if init_iterable is None:
+            init_iterable = []
+        self.data = []
+        self.selection = None
+        if init_iterable:
+            self.data = [self._validate_value(i) for i in init_iterable]
+        self._fire("reset", None)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __delitem__(self, key):
+        if type(key) is slice:
+            del self.data[key]
+            self._fire("reset", key)
+        else:
+            key = self._natural_index(key)
+            del self.data[key]
+            self._fire("delete", key)
+
+    def __setitem__(self, key, value):
+        if type(key) is slice:
+            for i in value:
+                self._validate_value(i)
+            self.data[key] = value
+            self._fire("reset", key)
+        else:
+            self.data[key] = self._validate_value(value)
+            self._fire("update", self._natural_index(key))
+
+    def insert(self, key, value):
+        self.data.insert(key, self._validate_value(value))
+        self._fire("insert", self._natural_index(key))
+
+    def append(self, value):
+        self.data.append(self._validate_value(value))
+        self._fire("insert", len(self.data) - 1)
+        self.select(len(self.data) - 1)
+
+    def select(self, index):
+        if self.selection == index:
+            return
+        self.selection = index
+        self._fire("select", index)
+
+    def _validate_value(self, val):
+        """
+            The members may only be "primitive" types (int, str and such),
+            or tuples of primitive types.
+        """
+        if type(val) in (int, long, float, str, unicode, ):
+            return val
+        if isinstance(val, tuple) or isinstance(val, frozenset):
+            for i in val:
+                self._validate_value(i)
+            return val
+        raise TypeError(
+            "Only number/strings and tuples/frozensets allowed here.",
+        )
+    def _natural_index(self, index):
+        """
+            If we get a negative index, we must convert it to the "natural"
+            0-based one.
+        """
+        if index < 0:
+            return len(self.data) + index
+        return index
+
+    def __str__(self):
+        return str(self.data)
+    def __unicode__(self):
+        return unicode(self.data)
+
---
+++ step02/tkl.py
@@ -0,0 +1,134 @@
+"""
+    A micro tk app with a list driven by the model.
+"""
+import logging
+
+from Tkinter import *
+from ttk import Treeview
+
+import hotlist
+
+class MVCList(Treeview):
+    def __init__(self, parent, model):
+        Treeview.__init__(
+            self,
+            parent,
+            columns=["COL0", "COL1"],
+            selectmode="browse",
+        )
+        self.heading(0, text="Number")
+        self.heading(1, text="Text")
+        self["show"] = "headings"
+        self.counter = 0
+        self.bind("<<TreeviewSelect>>", self.on_select)
+        self.model = model
+        self.model.add_listener(self.on_model)
+
+    def on_select(self, event):
+        sel = self.selection()
+        if not sel:
+            self.model.select(None)
+        else:
+            self.model.select(self.index(sel[0]))
+
+    def on_model(self, event_source, event_name, data):
+        """
+            An event has been "fired" in the model. It is our responsibility
+            to handle only the events we care about.
+        """
+        handler = "handle_%s" % event_name
+
+        if handler in dir(self):
+            getattr(self, handler)(event_source, event_name, data)
+        else:
+            logging.warn("Unhandled in MVCList: %s", event_name)
+
+    def handle_reset(self, event_source, event_name, data):
+        """
+            Rebuild the list's contents.
+        """
+        self.delete(self.get_children())
+        for (i, data) in enumerate(self.event_source):
+            self.add_item(i, data)
+
+    def handle_insert(self, event_source, event_name, data):
+        """
+            A handler for the item insertion.
+        """
+        self.add_item(data, event_source[data])
+
+    def handle_select(self, event_source, event_name, data):
+        """
+            A handler for the selection.
+        """
+        self.selection_set(self.get_children()[data])
+
+    def handle_delete(self, event_source, event_name, data):
+        """
+            A handler for the item deletion.
+        """
+        kids = self.get_children()
+        self.delete(kids[data])
+
+    def add_item(self, index, data):
+        """
+            Inserts an item at the desired position.
+        """
+        item = self.insert("", index, text="")
+        self.counter += 1
+        for (index, value) in enumerate(data):
+            self.set(item, index, value)
+
+class Application(Frame):
+    def add_item(self):
+        """
+            The handler for the add button.
+        """
+        self.counter += 1
+        self.list_model.append((str(self.counter), "hello"))
+
+    def del_item(self):
+        """
+            The handler for the delete button.
+        """
+        sel = self.list_view.selection()
+        if not sel:
+            return
+        sel_index = self.list_view.index(sel[0])
+        del self.list_model[sel_index]
+
+    def createWidgets(self):
+        """
+            Layout the window.
+        """
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "Quit"
+        self.QUIT["command"] =  self.quit
+
+        self.add_btn = Button(self)
+        self.add_btn["text"] = "Add an item"
+        self.add_btn["command"] = self.add_item
+
+        self.del_btn = Button(self)
+        self.del_btn["text"] = "Delete selected"
+        self.del_btn["command"] = self.del_item
+
+        self.QUIT.pack({"side": "bottom"})
+        self.del_btn.pack({"side": "bottom"})
+        self.add_btn.pack({"side": "bottom"})
+
+        self.list_model = hotlist.HotList()
+        self.list_view = MVCList(self, self.list_model)
+        self.list_view.pack({"side": "top"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.counter = 0
+        self.pack()
+        self.createWidgets()
+
+ROOT = Tk()
+APP = Application(master=ROOT)
+APP.mainloop()
+ROOT.destroy()
+
